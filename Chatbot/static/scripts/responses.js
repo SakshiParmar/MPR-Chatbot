dfb21884bcaa5425d806271f9d2402ec9a7b0554
pri <prishapanchmia@gmail.com>
2022-04-10 20:08:11 +0530

    frontend

--- Chatbot/static/scripts/responses.js
+++ Chatbot/static/scripts/responses.js
@@ -1,27 +1,16 @@
 function getBotResponse(input) {
-    //rock paper scissors
-    if (input == "rock") {
-        return "paper";
-    } else if (input == "paper") {
-        return "scissors";
-    } else if (input == "scissors") {
-        return "rock";
-    }
-
-    // Simple responses
+    
     if (input == "hello") {
         return "Hello there! How are you feeling today?";
     } else if (input == "goodbye") {
         return "Talk to you later!";
     } else if (input == "How are you?") {
         return "I'm good";
-    } else if (input == "goodbye") {
+    } else if (input == "bye") {
         return "Talk to you later!";
-    } else if (input == "What's your fav phrase?") {
-        return "Very good prisha and aryan";
     } else if (input == "I'm good") {
-        return "Acha laga sunke";
+        return "That is great!";
     } else {
-        return "Try asking something else!";
+        return "I dont understand, ask me something else!";
     }
 }
